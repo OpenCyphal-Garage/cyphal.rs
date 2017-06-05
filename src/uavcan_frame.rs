0e8f4635301289893c221627a5fc604791a00fe4
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-06-05 02:07:28 +0200

    Generalized Can id to MessageFrameHeader

--- src/uavcan_frame.rs
+++ src/uavcan_frame.rs
@@ -65,6 +65,25 @@ impl From<u8> for TailByte {
     }
 }
 
+pub trait TransportFrameHeader {
+    fn to_id(&self) -> u32;
+    fn set_priority(&mut self, priority: u8);
+    fn get_priority(&self) -> u8;
+}
+
+pub trait UavcanTransmitable : UavcanIndexable {
+    fn get_signature() -> u64;
+    fn get_header(&self) -> &TransportFrameHeader;
+
+}
+
+pub enum DataTypeSignature {
+    Message,
+    Service,
+    AnonymousMessage,
+}
+
+
     
 
 struct MessageFrameHeader {
@@ -77,7 +96,6 @@ struct AnonymousFrameHeader {
     priority: u8,
     discriminator: u16,
     type_id: u8,
-    source_node: u8,
 }
 
 struct ServiceFrameHeader {
@@ -88,23 +106,42 @@ struct ServiceFrameHeader {
     source_node: u8,
 }
 
-impl ToCanID for MessageFrameHeader {
-    fn to_can_id(&self) -> CanID {
-        return CanID::Extended( ((self.priority as u32) << 24)&(0x1f000000) | ((self.type_id as u32) << 8)&(0x00ffff00) | (0u32 << 7) | ((self.source_node as u32))&(0x0000007f) );
+impl TransportFrameHeader for MessageFrameHeader {
+    fn to_id(&self) -> u32 {
+        return ((self.priority as u32) << 24)&(0x1f000000) | ((self.type_id as u32) << 8)&(0x00ffff00) | (0u32 << 7) | ((self.source_node as u32))&(0x0000007f);
+    }
+    fn set_priority(&mut self, priority: u8) {
+        self.priority.set_bit_range(0..5, priority);
+    }
+    fn get_priority(&self) -> u8 {
+        self.priority.bit_range(0..5)
+    }    
+}
+
+impl TransportFrameHeader for AnonymousFrameHeader {
+    fn to_id(&self) -> u32 {
+        return ((self.priority as u32) << 24)&(0x1f000000) | ((self.discriminator as u32) << 10)&(0x00fffc00) | ((self.type_id as u32) << 10)&(0x00000300) | (0u32 << 7);
+    }
+    fn set_priority(&mut self, priority: u8) {
+        self.priority.set_bit_range(0..5, priority);
+    }
+    fn get_priority(&self) -> u8{
+        self.priority.bit_range(0..5)
     }
 }
 
-impl ToCanID for AnonymousFrameHeader {
-    fn to_can_id(&self) -> CanID {
-        return CanID::Extended( ((self.priority as u32) << 24)&(0x1f000000) | ((self.discriminator as u32) << 10)&(0x00fffc00) | ((self.type_id as u32) << 10)&(0x00000300) | (0u32 << 7) | ((self.source_node as u32))&(0x0000007f) );
+impl TransportFrameHeader for ServiceFrameHeader {
+    fn to_id(&self) -> u32 {
+        return ((self.priority as u32) << 24)&(0x1f000000) | ((self.type_id as u32) << 16)&(0x00ff0000) | ((self.request_not_response as u32) << 15) | ((self.destination_node as u32) << 8)&(0x00007f00) | (1u32 << 7) | ((self.source_node as u32) << 0)&(0x0000007f);
+    }
+    fn set_priority(&mut self, priority: u8) {
+        self.priority.set_bit_range(0..5, priority);
+    }
+    fn get_priority(&self) -> u8{
+        self.priority.bit_range(0..5)
     }
 }
 
-impl ToCanID for ServiceFrameHeader {
-    fn to_can_id(&self) -> CanID {
-        return CanID::Extended( ((self.priority as u32) << 24)&(0x1f000000) | ((self.type_id as u32) << 16)&(0x00ff0000) | ((self.request_not_response as u32) << 15) | ((self.destination_node as u32) << 8)&(0x00007f00) | (1u32 << 7) | ((self.source_node as u32) << 0)&(0x0000007f) );
-    }
-}
 
 
 #[derive(Debug)]
@@ -207,8 +244,8 @@ impl<T: UavcanIndexable> Parser<T> {
 
     
 }
-                                                    
- 
+
+
 
 
 #[cfg(test)]
@@ -216,7 +253,6 @@ mod tests {
     use uavcan_frame::*;
     use core::fmt::*;
     use crc;
-
 
     #[test]
     fn uavcan_sized_length_derivation() {
