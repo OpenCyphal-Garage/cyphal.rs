044fbb4800b046ebe5527e2dbb1763bbd394f311
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-06-05 16:53:38 +0200

    Moved out message builder to its own submodule

--- src/lib.rs
+++ src/lib.rs
@@ -191,67 +191,6 @@ impl<H: TransportFrameHeader, B: UavcanIndexable> UavcanTransmitable for UavcanF
 }
 
 
-#[derive(Debug)]
-pub enum BuilderError {
-    FirstFrameNotStartFrame,
-    BlockAddedAfterEndFrame,
-    ToggleError,
-    CRCError,
-    FormatError,
-}
-
-
-pub struct MessageBuilder<T: UavcanTransmitable> {
-    parser: Parser<T>,
-    started: bool,
-    crc: u16,
-    toggle: bool,
-    transfer_id: u8,    
-}
-
-impl<T:UavcanTransmitable> MessageBuilder<T> {
-    fn from_structure(structure: T) -> Self {
-        MessageBuilder{
-            parser: Parser::from_structure(structure),
-            started: false,
-            crc: 0x00,
-            toggle: false,
-            transfer_id: 0x00,
-        }
-    }
-    
-    fn add_frame<F: TransportFrame>(mut self, frame: F) -> Result<Self, BuilderError> {
-        if !self.started {
-            if !frame.is_start_frame() {
-                return Err(BuilderError::FirstFrameNotStartFrame);
-            }
-            if frame.get_tail_byte().toggle {
-                return Err(BuilderError::ToggleError);
-            }
-            self.toggle = false;
-            self.crc.set_bit_range(0..8, frame.get_data()[0] as u16)
-                .set_bit_range(8..16, frame.get_data()[1] as u16); 
-            self.transfer_id = frame.get_tail_byte().transfer_id;
-            self.started = true;
-        }
-
-        let payload = if frame.is_start_frame() && !frame.is_end_frame() {
-            &frame.get_data()[2..frame.get_data().len()-1]
-        } else {
-            &frame.get_data()[0..frame.get_data().len()-1]
-        };
-
-        self.parser = match self.parser.parse(payload) {
-            Ok(x) => x,
-            Err(ParseError::StructureExhausted) => return Err(BuilderError::FormatError),
-        };
-            
-
-        return Ok(self);
-    }
-            
-                
-}
 
 
 #[cfg(test)]
