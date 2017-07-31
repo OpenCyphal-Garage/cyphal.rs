41cfbabe5bfb501d7f0aa26b2ee5c77c3ba92752
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-07-31 23:28:07 +0200

    Added function to get next transport frame from frame_generator

--- src/frame_generator.rs
+++ src/frame_generator.rs
@@ -1,4 +1,5 @@
 use {
+    TailByte,
     TransportFrame,
     UavcanFrame,
     UavcanHeader,
@@ -6,6 +7,7 @@ use {
 };
 
 use serializer::{
+    SerializationResult,
     Serializer,
 };
 
@@ -31,4 +33,34 @@ impl<B: UavcanIndexable> FrameGenerator<B> {
         }
     }
     
+    pub fn next_transport_frame<T: TransportFrame>(&mut self) -> Option<T> {
+        let remaining_bits = self.serializer.remaining_bits();
+        let max_data_length = T::max_data_length();
+        let max_payload_length = max_data_length - 1;
+        let mut transport_frame = T::with_length(self.id, max_data_length);
+
+        
+        let first_of_multi_frame = !self.started && (remaining_bits > max_payload_length*8);
+
+        if remaining_bits == 0 {
+            return None;
+        } else if first_of_multi_frame {
+            // TODO: calc crc
+            self.serializer.serialize(&mut transport_frame.data_as_mut()[2..max_data_length-1]);
+            transport_frame.data_as_mut()[max_data_length-1] = TailByte{start_of_transfer: !self.started, end_of_transfer: false, toggle: self.toggle, transfer_id: self.transfer_id}.into();
+        } else {
+            if let SerializationResult::Finished(i) = self.serializer.serialize(&mut transport_frame.data_as_mut()[0..max_data_length-1]){
+                let frame_length = (i+7)/8 + 1;
+                transport_frame.set_data_length(frame_length);
+                transport_frame.data_as_mut()[frame_length-1] = TailByte{start_of_transfer: !self.started, end_of_transfer: true, toggle: self.toggle, transfer_id: self.transfer_id}.into();
+            }
+        }
+
+        self.started = true;
+        self.toggle = !self.toggle;
+        
+        return Some(transport_frame);
+    }
 }
+
+
