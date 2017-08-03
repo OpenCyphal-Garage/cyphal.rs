d9389e10f664062e0c7b6aa2fdf00df34e27573f
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-08-04 01:12:45 +0200

    Added function for serializer to calculate crc

--- src/serializer.rs
+++ src/serializer.rs
@@ -2,7 +2,12 @@ use {
     UavcanIndexable,
 };
 
-use bit_field::BitArray;
+use crc;
+
+use bit_field::{
+    BitField,
+    BitArray,
+};
 
 pub struct Serializer<T: UavcanIndexable> {
     structure: T,
@@ -124,7 +129,59 @@ impl<T: UavcanIndexable> Serializer<T> {
             }
         }
     }
-    
+
+    pub fn crc(&self, data_type_signature: u64) -> u16 {
+        let mut field_index = 0;
+        let mut type_index = 0;
+
+        let mut crc = 0xffff;
+        let mut remaining_data: u8 = 0;
+        let mut remaining_bits = 0;
+
+        for i in 0..4 {
+            crc = crc::add_byte(crc, &(data_type_signature.get_bits(8*i..8*(i+1)) as u8));;
+        }
+        
+        loop {
+            let primitive_type = self.structure.primitive_field(field_index).primitive_type(type_index);
+            let bit_length = primitive_type.bit_length();
+            let mut bit_index = 0;
+            let data = primitive_type.get_bits(0..bit_length);
+
+            if remaining_bits + bit_length < 8 {
+                remaining_data.set_bits(remaining_bits as u8..(remaining_bits+bit_length) as u8, data.get_bits(0..bit_length as u8) as u8);
+                bit_index = bit_length;
+            } else if remaining_bits != 0 {
+                crc::add_byte(crc, &0u8
+                              .set_bits(0..remaining_bits as u8, remaining_data.get_bits(0..remaining_bits as u8) as u8)
+                              .set_bits(remaining_bits as u8..8-remaining_bits as u8, data.get_bits(0..8-remaining_bits as u8) as u8)
+                              .get_bits(0..8)
+                );
+                bit_index += 8-remaining_bits;
+            }
+
+            while bit_length - bit_index >= 8 {
+                crc::add_byte(crc, &(data.get_bits(bit_index as u8..bit_index as u8+8) as u8));
+                bit_index += 8;
+            }
+
+                remaining_bits = bit_length-bit_index;
+            if bit_length-bit_index != 0 {
+                remaining_data = data.get_bits(bit_index as u8..bit_length as u8) as u8;
+            }
+                    
+            type_index += 1;
+            
+            if type_index >= self.structure.primitive_field(field_index).length() {
+                type_index = 0;
+                field_index += 1;
+            }
+            if field_index >= self.structure.number_of_primitive_fields() {
+                return crc;
+            }
+        }
+        
+    }
 }       
     
 
