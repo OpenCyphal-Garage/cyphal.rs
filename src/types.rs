3c2484cae97905e99ac211605d09a53b696aaae7
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-08-03 21:30:57 +0200

    Implement some instances of DynamicArray

--- src/types.rs
+++ src/types.rs
@@ -7,6 +7,7 @@ use {
     UavcanIndexable,
     UavcanPrimitiveField,
     UavcanPrimitiveType,
+    DynamicArray,
 };
 
 
@@ -199,9 +200,41 @@ pub struct Float64 {
     value: f64,
 }
 
-
-
-
+macro_rules! dynamic_array_def {
+    ($i:ident, $n:expr) => {
+        pub struct $i<T: UavcanPrimitiveType> {
+            current_size: usize,
+            data: [T; $n],
+        }
+        
+        impl<T: UavcanPrimitiveType + Copy> DynamicArray for $i<T> {
+            type Field = T;
+            
+            fn max_size() -> usize {$n}
+            fn with_data(data: &[T]) -> Self {
+                let mut data_t = [data[0]; $n];
+                for i in 0..data.len() {
+                    data_t[i] = data[i];
+                }
+                Self{
+                    current_size: data.len(),
+                    data: data_t,
+                }
+            }
+            fn set_length(&mut self, length: usize) {self.current_size = length;}
+            fn data(&self) -> &[T] {&self.data[0..self.current_size]}
+            fn data_as_mut(&mut self) -> &mut [T] {&mut self.data[0..self.current_size]}
+        }
+        
+        impl<T: UavcanPrimitiveType> UavcanPrimitiveField for $i<T> {
+            fn is_constant_size(&self) -> bool {false}
+            fn length(&self) -> usize {self.data.len()}
+            fn set_length(&mut self, length: usize) {self.current_size = length;}
+            fn primitive_type(&self, index: usize) -> &UavcanPrimitiveType {&self.data[index]}
+            fn primitive_type_as_mut(&mut self, index: usize) -> &mut UavcanPrimitiveType {&mut self.data[index]}
+        }
+    };
+}
 
 
 
@@ -383,3 +416,22 @@ impl_primitive_field!(Uint5);
 impl_primitive_field!(Uint8);
 impl_primitive_field!(Uint16);
 impl_primitive_field!(Uint32);
+
+dynamic_array_def!(DynamicArray2, 2);
+dynamic_array_def!(DynamicArray3, 3);
+dynamic_array_def!(DynamicArray4, 4);
+dynamic_array_def!(DynamicArray5, 5);
+dynamic_array_def!(DynamicArray6, 6);
+dynamic_array_def!(DynamicArray7, 7);
+dynamic_array_def!(DynamicArray8, 8);
+dynamic_array_def!(DynamicArray9, 9);
+dynamic_array_def!(DynamicArray10, 10);
+dynamic_array_def!(DynamicArray11, 11);
+dynamic_array_def!(DynamicArray12, 12);
+dynamic_array_def!(DynamicArray13, 13);
+dynamic_array_def!(DynamicArray14, 14);
+dynamic_array_def!(DynamicArray15, 15);
+dynamic_array_def!(DynamicArray16, 16);
+dynamic_array_def!(DynamicArray31, 31);
+dynamic_array_def!(DynamicArray32, 32);
+dynamic_array_def!(DynamicArray90, 90);
