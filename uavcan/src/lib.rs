94fb308fbc0b9a7ff30cebfa3f9c4abcaee33b56
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-08-18 15:52:00 +0200

    Added clippy as a feature

--- uavcan/src/lib.rs
+++ uavcan/src/lib.rs
@@ -1,4 +1,6 @@
 #![cfg_attr(not(feature="std"), no_std)]
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
 
 #[macro_use]
 extern crate uavcan_derive;
