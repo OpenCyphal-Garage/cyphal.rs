46b886038202fb9e0785b7e4aec1fd83957238f7
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-08-19 21:15:46 +0200

    Change char lit to byte lit, this will please the linter

--- uavcan/src/message_builder.rs
+++ uavcan/src/message_builder.rs
@@ -203,25 +203,25 @@ mod tests {
         message_builder = message_builder.add_frame(&CanFrame{
             id: CanID::Extended(LogMessageHeader::new(0, 32).id()),
             dlc: 8,
-            data: [crc.get_bits(0..8) as u8, crc.get_bits(8..16) as u8, 0u8.set_bits(0..3, 0).set_bits(3..8, 11).get_bits(0..8), 't' as u8, 'e' as u8, 's' as u8, 't' as u8, TailByte{start_of_transfer: true, end_of_transfer: false, toggle: false, transfer_id: 0}.into()],
+            data: [crc.get_bits(0..8) as u8, crc.get_bits(8..16) as u8, 0u8.set_bits(0..3, 0).set_bits(3..8, 11).get_bits(0..8), b't', b'e', b's', b't', TailByte{start_of_transfer: true, end_of_transfer: false, toggle: false, transfer_id: 0}.into()],
         }).unwrap();
         
         message_builder = message_builder.add_frame(&CanFrame{
             id: CanID::Extended(LogMessageHeader::new(0, 32).id()),
             dlc: 8,
-            data: [' ' as u8, 's' as u8, 'o' as u8, 'u' as u8, 'r' as u8, 'c' as u8, 'e' as u8, TailByte{start_of_transfer: false, end_of_transfer: false, toggle: true, transfer_id: 0}.into()],
+            data: [b' ', b's', b'o', b'u', b'r', b'c', b'e', TailByte{start_of_transfer: false, end_of_transfer: false, toggle: true, transfer_id: 0}.into()],
         }).unwrap();
         
         message_builder = message_builder.add_frame(&CanFrame{
             id: CanID::Extended(LogMessageHeader::new(0, 32).id()),
             dlc: 8,
-            data: ['t' as u8, 'e' as u8, 's' as u8, 't' as u8, ' ' as u8, 't' as u8, 'e' as u8, TailByte{start_of_transfer: false, end_of_transfer: false, toggle: false, transfer_id: 0}.into()],
+            data: [b't', b'e', b's', b't', b' ', b't', b'e', TailByte{start_of_transfer: false, end_of_transfer: false, toggle: false, transfer_id: 0}.into()],
         }).unwrap();
         
         message_builder = message_builder.add_frame(&CanFrame{
             id: CanID::Extended(LogMessageHeader::new(0, 32).id()),
             dlc: 3,
-            data: ['x' as u8, 't' as u8, TailByte{start_of_transfer: false, end_of_transfer: true, toggle: true, transfer_id: 0}.into(), 0, 0, 0, 0, 0],
+            data: [b'x', b't', TailByte{start_of_transfer: false, end_of_transfer: true, toggle: true, transfer_id: 0}.into(), 0, 0, 0, 0, 0],
         }).unwrap();
         
         assert_eq!(uavcan_frame, message_builder.build().unwrap());
