92f6dd192e7311a4e723751fc44b6cebf71bce89
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-09-15 22:18:10 +0200

    Implemented serialize for DynamicArrayLength

--- uavcan/src/serializer.rs
+++ uavcan/src/serializer.rs
@@ -27,10 +27,61 @@ struct SerializationBuffer<'a> {
     data: &'a mut [u8],
     bit_index: usize,
 }
- 
+
 pub trait Serialize {
     fn serialize(&self, start_bit: usize, buffer: &mut SerializationBuffer) -> SerializationResult;
     fn bits_remaining(&self, start_bit: usize) -> usize;
+}
+
+impl Serialize for DynamicArrayLength {
+    fn serialize(&self, start_bit: usize, buffer: &mut SerializationBuffer) -> SerializationResult {
+        let mut bits_serialized: usize = 0;
+        
+        let mut byte_start = buffer.bit_index / 8;
+        let odd_bits_start = buffer.bit_index % 8;
+        
+        // first get rid of the odd bits
+        if odd_bits_start != 0 && 8-odd_bits_start <= self.bit_length - start_bit {
+            buffer.data[byte_start].set_bits((odd_bits_start as u8)..8, self.current_length.get_bits((start_bit as u8)..(start_bit+8-odd_bits_start) as u8) as u8);
+            bits_serialized += 8-odd_bits_start;
+            buffer.bit_index += 8-odd_bits_start;
+            byte_start += 1;
+        } else if odd_bits_start != 0 && 8-odd_bits_start > self.bit_length - start_bit {
+            buffer.data[byte_start].set_bits((odd_bits_start as u8)..8, self.current_length.get_bits((start_bit as u8)..(start_bit + (self.bit_length - start_bit) ) as u8) as u8);
+            bits_serialized += self.bit_length - start_bit;
+            buffer.bit_index += self.bit_length - start_bit;
+            return SerializationResult::Finished(bits_serialized);
+        }
+        
+        for i in byte_start..buffer.data.len() {
+            let serialization_index = bits_serialized + start_bit;
+            let remaining_bits = self.bit_length - serialization_index;
+
+            if remaining_bits == 0 {
+                return SerializationResult::Finished(bits_serialized);
+            } else if remaining_bits <= 8 {
+                buffer.data[i] = self.current_length.get_bits((serialization_index as u8)..(serialization_index+remaining_bits) as u8) as u8;
+                buffer.bit_index += remaining_bits;
+                bits_serialized += remaining_bits;
+                return SerializationResult::Finished(bits_serialized);
+            } else {
+                buffer.data[i] = self.current_length.get_bits((serialization_index as u8)..(serialization_index+8) as u8) as u8;
+                buffer.bit_index += 8;
+                bits_serialized += 8;
+            }
+        }
+        
+        
+        SerializationResult::BufferFull(bits_serialized)
+
+    }
+
+    fn bits_remaining(&self, start_bit: usize) -> usize {
+        assert!(start_bit < self.bit_length);
+        self.bit_length - start_bit
+    }           
+
+
 }
 
 macro_rules! impl_serialize_for_primitive_type {
