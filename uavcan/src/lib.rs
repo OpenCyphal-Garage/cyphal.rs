94e1b624bbd654740bd2eae414fcd8d11d0b0d4b
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-09-19 13:44:56 +0200

    removed useless assert due to type limits

--- uavcan/src/lib.rs
+++ uavcan/src/lib.rs
@@ -132,7 +132,6 @@ pub trait UavcanStruct {
     }
     
     fn flattened_field(&self, field_number: usize) -> UavcanField {
-        assert!(field_number >= 0);
         assert!(field_number < self.flattened_fields_len());
         
         let mut accumulated_fields_len = 0;
@@ -161,7 +160,6 @@ pub trait UavcanStruct {
     
     
     fn flattened_field_as_mut(&mut self, field_number: usize) -> MutUavcanField {
-        assert!(field_number >= 0);
         assert!(field_number < self.flattened_fields_len());
         
         let mut accumulated_fields_len = 0;
