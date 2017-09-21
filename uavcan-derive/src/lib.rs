fcc80a5d1dc0a5452d34ca0f6a9dea81c8fc2c83
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-09-21 20:13:17 +0200

    removed derivation of field indexing

--- uavcan-derive/src/lib.rs
+++ uavcan-derive/src/lib.rs
@@ -247,57 +247,6 @@ fn impl_uavcan_struct(ast: &syn::DeriveInput) -> quote::Tokens {
         bit_length_builder
     };
     
-
-
-    let field_as_mut_body = {
-        let mut primitive_fields_builder = Tokens::new();
-        let mut primitive_fields_cases = Tokens::new();
-        
-        for (i, field) in variant_data.fields().iter().enumerate() {
-            let field_name = field.ident.as_ref().unwrap();
-            primitive_fields_cases.append(quote!{
-                #i => self.#field_name.as_mut_uavcan_field(),
-            });
-        }
-        
-        primitive_fields_cases.append(quote!{
-            x => panic!("The index: {}, is not a valid Uavcan Struct field", x),
-        });
-        
-        primitive_fields_builder.append(quote!{
-            match field_number {
-                #primitive_fields_cases
-            }
-        });
-
-        primitive_fields_builder
-    };
-        
-    let field_body = {
-        let mut primitive_fields_builder = Tokens::new();
-        let mut primitive_fields_cases = Tokens::new();
-
-        for (i, field) in variant_data.fields().iter().enumerate() {
-            let field_name = field.ident.as_ref().unwrap();
-            primitive_fields_cases.append(quote!{
-                #i => self.#field_name.as_uavcan_field(),
-            });
-        }
-        
-        primitive_fields_cases.append(quote!{
-            x => panic!("The index: {}, is not a valid Uavcan Struct field", x),
-        });
-        
-        primitive_fields_builder.append(quote!{
-            match field_number {
-                #primitive_fields_cases
-            }
-        });
-        
-        primitive_fields_builder
-    };
-    
-    
     
     quote!{
         impl UavcanStruct for #name {
