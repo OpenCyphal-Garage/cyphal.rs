74d3f05413c32de62b30a9e298b0d5a3261e8094
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-09-29 20:06:20 +0200

    Updated/Imporved documentation for TransferFrame

--- uavcan/src/transfer.rs
+++ uavcan/src/transfer.rs
@@ -19,34 +19,64 @@ pub struct FullTransferID {
     pub transfer_id: TransferID,
 }
 
-/// The TransportFrame is uavcan cores main interface to the outside world
+/// The TransferFrame is a CAN like frame that can be sent over a network
 ///
-/// This will in >99% of situations be a CAN2.0B frame
-/// But in theory both CAN-FD and other protocols which gives
-/// similar guarantees as CAN can also be used
+/// For a frame to work it need to have a 28 bit ID, and a payload of
+/// at least 4 bytes. Guarantee that frames are delivered in order
+/// and correctness check is needed as well.
+///
+/// The uavcan protocol defines how this works with a CAN2.0B frame
 pub trait TransferFrame {
+    /// Maximum data length the transfer protocol supports.
     const MAX_DATA_LENGTH: usize;
 
     /// Create a new TransferFrame with id: id, and length 0.
+    /// Data length can be changed with `set_data_length(&self)`.
+    /// Data can be changed with `data_as_mut(&mut self)`.
     fn new(id: TransferFrameID) -> Self;
+
+    /// Returns the 28 bit ID of this TransferFrame.
+    ///
+    /// When deciding which frame that will be transmitted,
+    /// the ID is used to prioritze (lower ID means higher priority)
+    fn id(&self) -> TransferFrameID;
+
+    /// Returns a slice with the data in this TransferFrame
+    ///
+    /// Length can be found by checking the length
+    /// of this slice `self.data().len()`
+    fn data(&self) -> &[u8];
+
+    /// Returns a mutable slice with the data in this TransferFrame
+    /// use this method to set/change the data inside this TransferFrame
+    fn data_as_mut(&mut self) -> &mut[u8];
+
+    /// Set the data length of this TransferFrame
+    ///
+    /// ## Panics
+    /// `set_data_lengt(&mut self, length: usize)` should panic if `length > T::MAX_DATA_LENGTH`
+    fn set_data_length(&mut self, length: usize);
     
+    /// Returns the tail byte of the TransferFrame assuming the current length
     fn tail_byte(&self) -> TailByte {
         TailByte::from(*self.data().last().unwrap())
     }
+
+    /// Checks the tail byte if this frame is a start frame and return the result
     fn is_start_frame(&self) -> bool {
         self.tail_byte().start_of_transfer()
     }
+    
+    /// Checks the tail byte if this frame is an end frame and return the result
     fn is_end_frame(&self) -> bool {
         self.tail_byte().end_of_transfer()
     }
+    
+    /// Checks the tail byte if this is both a start frame and an end frame and return the result
     fn is_single_frame(&self) -> bool {
         self.is_end_frame() && self.is_start_frame()
     }
 
-    fn set_data_length(&mut self, length: usize);
-    fn data(&self) -> &[u8];
-    fn data_as_mut(&mut self) -> &mut[u8];
-    fn id(&self) -> TransferFrameID;
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
