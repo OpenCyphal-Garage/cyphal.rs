7ff2f421218ef7c20e7e6cac89006f2200d51abd
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-14 15:30:38 +0200

    Added function for parsing const name

--- dsdl/src/parse.rs
+++ dsdl/src/parse.rs
@@ -13,7 +13,12 @@ use nom::{
 named!(comment<Comment>, map!(map_res!(preceded!(tag!("#"), not_line_ending), str::from_utf8), Comment::from));
 
 named!(field_name<Ident>, map!(map_res!(
-    verify!(take_while!(is_allowed_in_field_name), |x:&[u8]| is_lowercase(x[0])),
+    verify!(take_while!(is_allowed_in_field_name), |x:&[u8]| is_lowercase_char(x[0])),
+    str::from_utf8), Ident::from)
+);
+
+named!(const_name<Ident>, map!(map_res!(
+    verify!(take_while!(is_allowed_in_const_name), |x:&[u8]| is_uppercase_char(x[0])),
     str::from_utf8), Ident::from)
 );
 
@@ -28,16 +33,25 @@ named!(primitive_type<PrimitiveType>, map!(map_res!(
 
 
 
-fn is_lowercase(chr: u8) -> bool {
+fn is_lowercase_char(chr: u8) -> bool {
     chr >= b'a' && chr <= b'z'
+}
+
+fn is_uppercase_char(chr: u8) -> bool {
+    chr >= b'A' && chr <= b'Z'
 }
 
 fn is_numeric(chr: u8) -> bool {
     chr >= b'0' && chr <= b'9'
 }
 
+
 fn is_allowed_in_field_name(chr: u8) -> bool {
-    is_lowercase(chr) || is_numeric(chr) || chr == b'_'
+    is_lowercase_char(chr) || is_numeric(chr) || chr == b'_'
+}
+    
+fn is_allowed_in_const_name(chr: u8) -> bool {
+    is_uppercase_char(chr) || is_numeric(chr) || chr == b'_'
 }
     
         
@@ -66,6 +80,15 @@ mod tests {
         assert_eq!(field_name(&b"variable23"[..]), IResult::Done(&b""[..], Ident(String::from("variable23"))));
         assert_eq!(field_name(&b"var_iable23"[..]), IResult::Done(&b""[..], Ident(String::from("var_iable23"))));
         assert!(field_name(&b"2variable23"[..]).is_err());
+    }
+
+    #[test]
+    fn parse_const_name() {
+        assert_eq!(const_name(&b"CONST"[..]), IResult::Done(&b""[..], Ident(String::from("CONST"))));
+        assert_eq!(const_name(&b"CONST23"[..]), IResult::Done(&b""[..], Ident(String::from("CONST23"))));
+        assert_eq!(const_name(&b"CON_ST"[..]), IResult::Done(&b""[..], Ident(String::from("CON_ST"))));
+        assert_eq!(const_name(&b"CON_ST1_2345"[..]), IResult::Done(&b""[..], Ident(String::from("CON_ST1_2345"))));
+        assert!(const_name(&b"2CON"[..]).is_err());
     }
 
     #[test]
