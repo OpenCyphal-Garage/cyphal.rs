fac2560aae133dde9b5369b7a9f20834be93834d
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-17 20:50:47 +0200

    Added documentation for cast mode

--- dsdl_parser/src/lib.rs
+++ dsdl_parser/src/lib.rs
@@ -325,9 +325,20 @@ pub enum Const {
     Char(String),
 }
 
+/// Cast mode defines the rules of conversion from the native value of a certain programming language to the serialized field value.
+///
+/// Cast mode may be left undefined, in which case the default will be used.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum CastMode {
+    /// This is the default cast mode, which will be used if the attribute definition does not specify the cast mode explicitly.
+    ///
+    /// For integers, it prevents an integer overflow - for example, attempting to write 0x44 to a 4-bit field will result in a bitfield value of 0x0F.
+    /// For floating point values, it prevents overflow when casting to a lower precision floating point representation -
+    /// for example, 65536.0 will be converted to a float16 as 65504.0; infinity will be preserved.
     Saturated,
+
+    ///  For integers, it discards the excess most significant bits - for example, attempting to write 0x44 to a 4-bit field will produce 0x04.
+    /// For floating point values, overflow during downcasting will produce an infinity.
     Truncated,
 }
 
