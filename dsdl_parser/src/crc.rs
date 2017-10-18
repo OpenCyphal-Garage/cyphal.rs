4722b31601eb7de4cdc9a2eb7f2b147670a93cb1
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-18 17:27:14 +0200

    The CRC module needed to be public

--- dsdl_parser/src/crc.rs
+++ dsdl_parser/src/crc.rs
@@ -1,16 +1,16 @@
 
 
-struct CRC64WE(u64);
+pub struct CRC64WE(u64);
 
 impl CRC64WE {
     const MASK: u64 = 0xFFFFFFFFFFFFFFFF;
     const POLY: u64 = 0x42F0E1EBA9EA3693;
 
-    fn new() -> CRC64WE {
+    pub fn new() -> CRC64WE {
         CRC64WE(CRC64WE::MASK)
     }
 
-    fn add(&mut self, bytes: &[u8]) {
+    pub fn add(&mut self, bytes: &[u8]) {
         let CRC64WE(ref mut crc) = *self;
         for byte in bytes {
             *crc ^= (u64::from(*byte) << 56) & CRC64WE::MASK;
@@ -24,7 +24,7 @@ impl CRC64WE {
         }
     }
 
-    fn value(&self) -> u64 {
+    pub fn value(&self) -> u64 {
         (self.0 & CRC64WE::MASK) ^ CRC64WE::MASK
     }
 }
