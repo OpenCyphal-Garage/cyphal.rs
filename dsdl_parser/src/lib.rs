dbd072adb04026652381f9ed5427026f21313af9
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-19 16:54:37 +0200

    Improved module level docs

--- dsdl_parser/src/lib.rs
+++ dsdl_parser/src/lib.rs
@@ -2,13 +2,47 @@
 //!
 //! For full description have a look at the [specification](http://uavcan.org/Specification/3._Data_structure_description_language/)
 //!
-//! ## Parse DSDL
+//! ## Examples
+//! ### Parse DSDL directory
+//!
 //! ```
 //! use dsdl_parser::DSDL;
 //!
 //! assert!(DSDL::read("tests/dsdl/uavcan").is_ok());
 //!
 //! ```
+//!
+//! ### Parse single file
+//!
+//! ```
+//! use dsdl_parser::DSDL;
+//! 
+//! assert!(DSDL::read("tests/dsdl/uavcan/protocol/341.NodeStatus.uavcan").is_ok());
+//! 
+//! ```
+//!
+//! ### Display a file
+//!
+//! ```
+//! use dsdl_parser::DSDL;
+//!
+//! let dsdl = DSDL::read("./tests/dsdl/uavcan/").unwrap();
+//!
+//! println!("{}", dsdl.get_file("uavcan.protocol.GetNodeInfo").unwrap());
+//! 
+//! ```
+//!
+//! ### Calculate data type signature
+//!
+//! ```
+//! use dsdl_parser::DSDL;
+//!
+//! let dsdl = DSDL::read("./tests/dsdl/uavcan/").unwrap();
+//!
+//! assert_eq!(dsdl.data_type_signature("uavcan.protocol.GetNodeInfo").unwrap(), 0xee468a8121c46a9e);
+//! ```
+
+
 
 #[macro_use]
 extern crate nom;
