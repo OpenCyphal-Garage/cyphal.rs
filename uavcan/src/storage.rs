14923e68338570e4a32e86f7258191260aaa1ca6
Kjetil Kjeka <kjetilkjeka@gmail.com>
2018-08-06 18:27:15 +0200

    Added a storage implementation that uses the heap. Only usable with std for the moment but can be extended to alloc when that is stabilized

--- uavcan/src/storage.rs
+++ uavcan/src/storage.rs
@@ -2,10 +2,21 @@
 //!
 //! For the most basic use cases the provided defaults will most likely be sufficient and this module can be ignored.
 
+#[cfg(feature="std")]
+use std::sync::{
+    Mutex,
+    Arc,
+    Weak,
+};
+
+#[cfg(feature="std")]
+use std::collections::BinaryHeap;
+
 use transfer::TransferFrame;
 use transfer::TransferFrameID;
 use transfer::FullTransferID;
 use transfer::TransferFrameIDFilter;
+use transfer::Priority;
 
 #[derive(Debug, PartialEq)]
 pub enum StorageError {
@@ -66,4 +77,127 @@ pub trait InterfaceStorageHandle<F: TransferFrame> {
     ///
     /// This is the same as calling `insert_interface_queue` on the `Storage` which this handle is associated with.
     fn push(&self, frame: F) -> Result<(), StorageError>;
+}
+
+
+#[cfg(feature="std")]
+pub struct HeapStorage<F: TransferFrame> {
+    subscriber_list: Mutex<Vec<SubscriberListEntry<F>>>,
+    interface_list: Mutex<Vec<InterfaceListEntry<F>>>,
+}
+
+#[cfg(feature="std")]
+struct SubscriberListEntry<F: TransferFrame> {
+    filter: TransferFrameIDFilter,
+    storage: Weak<Mutex<Vec<F>>>,
+}
+
+#[cfg(feature="std")]
+struct InterfaceListEntry<F: TransferFrame> {
+    storage: Weak<Mutex<BinaryHeap<Priority<F>>>>,
+}
+
+#[cfg(feature="std")]
+pub struct HeapSubscriberStorage<F> {
+    storage: Arc<Mutex<Vec<F>>>,
+}
+
+#[cfg(feature="std")]
+pub struct HeapInterfaceStorage<F> {
+    storage: Arc<Mutex < BinaryHeap < Priority < F > > > >,
+}
+
+#[cfg(feature="std")]
+impl<F: TransferFrame + Clone> Storage<F> for HeapStorage<F> {
+    type SubscriberStorageHandle = HeapSubscriberStorage<F>;
+    type InterfaceStorageHandle = HeapInterfaceStorage<F>;
+
+    fn new() -> Self {
+        HeapStorage {
+            subscriber_list: Mutex::new(Vec::new()),
+            interface_list: Mutex::new(Vec::new()),
+        }
+    }
+
+    fn subscribe_to(&self, filter: TransferFrameIDFilter) -> Self::SubscriberStorageHandle {
+        let storage = Arc::new(Mutex::new(Vec::new()));
+
+        let subsciber_list_entry = SubscriberListEntry {
+            filter,
+            storage: Arc::downgrade(&storage),
+        };
+
+        let subscriber_handle = HeapSubscriberStorage {
+            storage,
+        };
+
+        // TODO: attempt to replace old (weak pointers that turns into None) list entries before creating new
+        self.subscriber_list.lock().unwrap().push(subsciber_list_entry);
+        subscriber_handle
+    }
+
+    fn new_interface(&self) -> Self::InterfaceStorageHandle {
+        let storage = Arc::new(Mutex::new(BinaryHeap::new()));
+
+        let interface_list_entry = InterfaceListEntry {
+            storage: Arc::downgrade(&storage),
+        };
+
+        let interface_handle = HeapInterfaceStorage {
+            storage,
+        };
+
+        self.interface_list.lock().unwrap().push(interface_list_entry);
+        interface_handle
+    }
+
+
+    fn insert_subscriber_queue(&self, frame: F) -> Result<(), StorageError> {
+        for storage in self.subscriber_list.lock().unwrap().iter().filter(|x| x.filter.is_match(frame.id())).filter_map(|x| x.storage.upgrade()) {
+            storage.lock().unwrap().push(frame.clone());
+        }
+        Ok(())
+    }
+
+    fn insert_interface_queue(&self, frame: F) -> Result<(), StorageError> {
+        for storage in self.interface_list.lock().unwrap().iter().filter_map(|x| x.storage.upgrade()) {
+            storage.lock().unwrap().push(Priority(frame.clone()));
+        }
+        Ok(())
+    }
+}
+
+#[cfg(feature="std")]
+impl<F: TransferFrame> SubscriberStorageHandle<F> for HeapSubscriberStorage<F> {
+    fn remove(&self, identifier: &TransferFrameID) -> Option<F> {
+        let mut queue = self.storage.lock().unwrap();
+        let pos = queue.iter().position(|x| x.id() == *identifier)?;
+        Some(queue.remove(pos))
+    }
+
+    fn find_id<P>(&self, mut predicate: P) -> Option<FullTransferID>
+        where P: FnMut(&F) -> bool {
+        Some(self.storage.lock().unwrap().iter().find(|x: &&F| predicate(*x))?.full_id())
+    }
+
+    fn retain<P>(&self, predicate: P)
+        where P: FnMut(&F) -> bool {
+        self.storage.lock().unwrap().retain(predicate)
+    }
+}
+
+#[cfg(feature="std")]
+impl<F: TransferFrame> InterfaceStorageHandle<F> for HeapInterfaceStorage<F> {
+    fn pop(&self) -> Option<F> {
+        Some(self.storage.lock().unwrap().pop()?.0)
+    }
+
+    fn max_priority(&self) -> Option<TransferFrameID> {
+        Some(self.storage.lock().unwrap().peek()?.0.id())
+    }
+
+    fn push(&self, frame: F) -> Result<(), StorageError> {
+        self.storage.lock().unwrap().push(Priority(frame));
+        Ok(())
+    }
 }
