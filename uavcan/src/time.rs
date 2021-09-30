81463f6fb659b474deecd9fc9a1ea56b6afdbfac
Alexander Hübener <alexander.huebener@it-designers.de>
2021-10-01 01:37:28 +0200

    moved testClock and stdClock in own mods

--- uavcan/src/time.rs
+++ uavcan/src/time.rs
@@ -1,41 +1,72 @@
-use core::{
-    hash::Hash,
-    ops::{Add, AddAssign},
-};
+#[cfg(test)]
+pub use test_clock::TestClock;
 
-use embedded_time::{duration::Duration, fixed_point::FixedPoint, Clock, ConversionError, TimeInt};
+#[cfg(feature = "std")]
+pub use std_clock::Clock;
 
-#[derive(Debug)]
-pub struct TestClock<TickType: TimeInt + AddAssign>(TickType);
+#[cfg(test)]
+mod test_clock {
+    use core::{
+        hash::Hash,
+        ops::{Add, AddAssign},
+    };
 
-impl<TickType: TimeInt + AddAssign> TestClock<TickType>
-where
-    Self: Clock,
-{
-    pub fn add_duration<D: Duration + FixedPoint>(
-        &mut self,
-        duration: D,
-    ) -> Result<(), ConversionError>
+    use embedded_time::{
+        duration::Duration, fixed_point::FixedPoint, Clock, ConversionError, TimeInt,
+    };
+
+    #[derive(Debug)]
+    pub struct TestClock<TickType: TimeInt + AddAssign>(TickType);
+
+    impl<TickType: TimeInt + AddAssign> TestClock<TickType>
     where
-        TickType: From<<D as FixedPoint>::T>,
+        Self: Clock,
     {
-        self.0 += duration.to_generic(Self::SCALING_FACTOR)?.integer();
-        Ok(())
+        pub fn add_duration<D: Duration + FixedPoint>(
+            &mut self,
+            duration: D,
+        ) -> Result<(), ConversionError>
+        where
+            TickType: From<<D as FixedPoint>::T>,
+        {
+            self.0 += duration.to_generic(Self::SCALING_FACTOR)?.integer();
+            Ok(())
+        }
+    }
+
+    impl TestClock<u32> {
+        pub fn new() -> Self {
+            Self(0)
+        }
+    }
+
+    impl<TickType: TimeInt + Hash + Add + AddAssign> Clock for TestClock<TickType> {
+        type T = TickType;
+
+        const SCALING_FACTOR: embedded_time::rate::Fraction =
+            embedded_time::rate::Fraction::new(1, 1);
+
+        fn try_now(&self) -> Result<embedded_time::Instant<Self>, embedded_time::clock::Error> {
+            Ok(embedded_time::Instant::new(self.0))
+        }
     }
 }
 
-impl TestClock<u32> {
-    pub fn new() -> Self {
-        Self(0)
-    }
-}
+#[cfg(feature = "std")]
+mod std_clock {
+    pub struct StdClock;
 
-impl<TickType: TimeInt + Hash + Add + AddAssign> Clock for TestClock<TickType> {
-    type T = TickType;
+    impl embedded_time::Clock for StdClock {
+        type T = u64;
 
-    const SCALING_FACTOR: embedded_time::rate::Fraction = embedded_time::rate::Fraction::new(1, 1);
+        const SCALING_FACTOR: embedded_time::rate::Fraction =
+            embedded_time::rate::Fraction::new(1, 1);
 
-    fn try_now(&self) -> Result<embedded_time::Instant<Self>, embedded_time::clock::Error> {
-        Ok(embedded_time::Instant::new(self.0))
+        fn try_now(&self) -> Result<embedded_time::Instant<Self>, embedded_time::clock::Error> {
+            let now = std::time::Instant::now();
+            let time_passed = now.elapsed().as_secs();
+
+            Ok(embedded_time::Instant::new(time_passed))
+        }
     }
 }
