dd29103bd2c6e9bef858703fe39dff4d9bfde0d8
Alexander Hübener <alex.teamplayer@gmail.com>
2021-10-05 23:10:46 +0200

    use new time implementation

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,48 +1,57 @@
-use uavcan::{Node, Priority, Subscription, TransferKind, transfer::Transfer, types::TransferId};
+use embedded_time::duration::Milliseconds;
+use embedded_time::Clock;
 use uavcan::session::StdVecSessionManager;
-use uavcan::transport::can::{Can, CanMetadata, CanFrame as UavcanFrame};
+use uavcan::time::StdClock;
+use uavcan::transport::can::{Can, CanFrame as UavcanFrame, CanMetadata};
+use uavcan::{transfer::Transfer, types::TransferId, Node, Priority, Subscription, TransferKind};
 
-use socketcan::{CANFrame, CANSocket};
 use arrayvec::ArrayVec;
+use socketcan::{CANFrame, CANSocket};
 
 fn main() {
-    let mut session_manager = StdVecSessionManager::<CanMetadata>::new();
-    session_manager.subscribe(Subscription::new(
-        TransferKind::Message,
-        7509, // TODO check
-        7,
-        core::time::Duration::from_millis(500)
-    )).unwrap();
-    session_manager.subscribe(Subscription::new(
-        TransferKind::Message,
-        100,
-        200,
-        core::time::Duration::from_millis(500)
-    )).unwrap();
-    let mut node: Node<StdVecSessionManager<CanMetadata>, Can> = Node::new(Some(42), session_manager);
-
+    let clock = StdClock::new();
+    let mut session_manager = StdVecSessionManager::<CanMetadata, Milliseconds>::new();
+    session_manager
+        .subscribe(Subscription::new(
+            TransferKind::Message,
+            7509, // TODO check
+            7,
+            embedded_time::duration::Milliseconds(500),
+        ))
+        .unwrap();
+    session_manager
+        .subscribe(Subscription::new(
+            TransferKind::Message,
+            100,
+            200,
+            embedded_time::duration::Milliseconds(500),
+        ))
+        .unwrap();
+    let mut node = Node::<_, Can, StdClock>::new(Some(42), session_manager);
 
     let sock = CANSocket::open("vcan0").unwrap();
 
-    let mut last_publish = std::time::Instant::now();
+    let mut last_publish = clock.try_now().unwrap();
     let mut transfer_id: TransferId = 30;
 
-    sock.set_read_timeout(core::time::Duration::from_millis(100)).unwrap();
+    sock.set_read_timeout(std::time::Duration::from_millis(100))
+        .unwrap();
 
     loop {
         let socketcan_frame = sock.read_frame().ok();
 
         if let Some(socketcan_frame) = socketcan_frame {
-
             // Note that this exposes some things I *don't* like about the API
             // 1: we should have CanFrame::new or something
             // 2: I don't like how the payload is working
             let mut uavcan_frame = UavcanFrame {
-                timestamp: std::time::Instant::now(),
+                timestamp: clock.try_now().unwrap(),
                 id: socketcan_frame.id(),
                 payload: ArrayVec::new(),
             };
-            uavcan_frame.payload.extend(socketcan_frame.data().iter().copied());
+            uavcan_frame
+                .payload
+                .extend(socketcan_frame.data().iter().copied());
 
             let xfer = match node.try_receive_frame(uavcan_frame) {
                 Ok(xfer) => xfer,
@@ -72,14 +81,16 @@ fn main() {
             }
         }
 
-        if std::time::Instant::now() - last_publish > std::time::Duration::from_millis(500) {
+        if clock.try_now().unwrap() - last_publish
+            > embedded_time::duration::Generic::new(500, StdClock::SCALING_FACTOR)
+        {
             // Publish string
             let hello = "Hello Python!";
             let mut str = Vec::from([hello.len() as u8, 0]);
             str.extend_from_slice(hello.as_bytes());
 
             let transfer = Transfer {
-                timestamp: std::time::Instant::now(),
+                timestamp: clock.try_now().unwrap(),
                 priority: Priority::Nominal,
                 transfer_kind: TransferKind::Message,
                 port_id: 100,
@@ -93,7 +104,8 @@ fn main() {
             transfer_id = (std::num::Wrapping(transfer_id) + std::num::Wrapping(1)).0;
 
             for frame in node.transmit(&transfer).unwrap() {
-                sock.write_frame(&CANFrame::new(frame.id, &frame.payload, false, false).unwrap()).unwrap();
+                sock.write_frame(&CANFrame::new(frame.id, &frame.payload, false, false).unwrap())
+                    .unwrap();
 
                 //print!("Can frame {}: ", i);
                 //for byte in &frame.payload {
@@ -106,8 +118,7 @@ fn main() {
                 //}
             }
 
-            last_publish = std::time::Instant::now();
+            last_publish = clock.try_now().unwrap();
         }
-
     }
 }
