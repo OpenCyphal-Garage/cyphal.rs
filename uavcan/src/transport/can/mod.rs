9314485beac196b88720a4d5508b5152f253ed1a
David Lenfesty <lenfesty@ualberta.ca>
2021-10-10 15:00:01 -0600

    transport/can: clone legacy CAN into FD

--- uavcan/src/transport/can/mod.rs
+++ uavcan/src/transport/can/mod.rs
@@ -35,3 +35,60 @@ mod tests;
 pub use legacy::*;
 pub use bitfields::{CanMessageId, CanServiceId};
 
+use bitfields::TailByte;
+
+/// Keeps track of toggle bit and CRC during frame processing.
+#[derive(Debug)]
+pub struct CanMetadata {
+    toggle: bool,
+    crc: crc_any::CRCu16,
+}
+
+impl<C: embedded_time::Clock> crate::transport::SessionMetadata<C> for CanMetadata {
+    fn new() -> Self {
+        Self {
+            // Toggle starts off true, but we compare against the opposite value.
+            toggle: false,
+            crc: crc_any::CRCu16::crc16ccitt_false(),
+        }
+    }
+
+    fn update(&mut self, frame: &crate::internal::InternalRxFrame<C>) -> Option<usize> {
+        // Single frame transfers don't need to be validated
+        if frame.start_of_transfer && frame.end_of_transfer {
+            // Still need to truncate tail byte
+            return Some(frame.payload.len() - 1);
+        }
+
+        // CRC all but the tail byte
+        self.crc.digest(&frame.payload[0..frame.payload.len() - 1]);
+        self.toggle = !self.toggle;
+
+        let tail = TailByte(frame.payload[frame.payload.len() - 1]);
+
+        if tail.toggle() == self.toggle {
+            if tail.end_of_transfer() {
+                // Exclude CRC from data
+                Some(frame.payload.len() - 3)
+            } else {
+                // Just truncate tail byte
+                Some(frame.payload.len() - 1)
+            }
+        } else {
+            None
+        }
+    }
+
+    fn is_valid(&self, frame: &crate::internal::InternalRxFrame<C>) -> bool {
+        if frame.start_of_transfer && frame.end_of_transfer {
+            return true;
+        }
+
+        if self.crc.get_crc() == 0x0000u16 {
+            return true;
+        }
+
+        return false;
+    }
+}
+
