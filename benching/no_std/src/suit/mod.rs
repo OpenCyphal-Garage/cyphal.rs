a1a7eba249eae893affd6c5566eb16f105d0930e
Alexander Hübener <alex.teamplayer@gmail.com>
2021-11-04 14:00:14 +0100

    decr. round size to one

--- benching/no_std/src/suit/mod.rs
+++ benching/no_std/src/suit/mod.rs
@@ -3,6 +3,6 @@ pub mod send;
 pub mod test;
 
 const SAMPLE_SIZE: usize = 100;
-const ROUND_SIZE: usize = 10;
+const ROUND_SIZE: usize = 1;
 
 pub type Bencher<C> = crate::benching::bencher::Bencher<C, SAMPLE_SIZE, ROUND_SIZE>;
